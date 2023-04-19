9775a0ac3681d0340b1ab63b797f9908fad2419a
yaellanir <yaellanir@gmail.com>
2023-04-19 15:20:31 +0300

    adding facebook bost event embed and download pdf file once submiting email info

--- src/pages/EventsPage/EventsPage.jsx
+++ src/pages/EventsPage/EventsPage.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import './EventsPage.css'
+import React from "react";
+import "./EventsPage.css";
 
 function EventsPage() {
   return (
     <div id="events" className="events-wrapper">
-      <div className="eventsText">Events</div>
-      </div>
-  )
+      {/* <div className="eventsText">Events</div>
+      <div
+        className="fb-page"
+        data-tabs="events"
+        data-href="https://www.facebook.com/DefendIsraeliDemocracy"
+        // data-href="https://www.facebook.com/YoloBookStore"
+        data-width="380"
+        data-hide-cover="false"
+      ></div> */}
+      {/* <div
+        className="fb-page"
+        data-href="https://www.facebook.com/DefendIsraeliDemocracy"
+        // data-href="https://www.facebook.com/watch/?v=2185176135151232"
+        data-tabs="timeline"
+        data-width="380"
+        // data-height=""
+        data-small-header="true"
+        data-adapt-container-width="true"
+        data-hide-cover="true"
+        data-show-facepile="false"
+      >
+        <blockquote
+          cite="https://www.facebook.com/DefendIsraeliDemocracy"
+          className="fb-xfbml-parse-ignore"
+        >
+          <a href="https://www.facebook.com/DefendIsraeliDemocracy">
+            ‎נאבקים על הדמוקרטיה בבית Defend Israeli Democracy‎
+          </a>
+        </blockquote>
+      </div> */}
+      <div
+        className="fb-post"
+        data-href="https://www.facebook.com/watch/?v=2185176135151232"
+        data-width="380"
+      ></div>
+    </div>
+  );
 }
 
-export default EventsPage
+export default EventsPage;
--- src/pages/MailingPage/MailingPage.jsx
+++ src/pages/MailingPage/MailingPage.jsx
@@ -3,35 +3,22 @@ import Mailchimp from "./Mailchimp";
 import "./MailingPage.css";
 
 function MailingPage() {
-  const [modalOpen, setModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(true);
   return (
     <div id="mailing" className="mailing-wrapper">
       <div className="mailing-text">Connect With Us</div>
-      {!modalOpen && (
+      {/* {!modalOpen && (
         <button  className='primaryBtn' onClick={() => setModalOpen(true)}>Sign Up</button>
-      )}
+      )} */}
       {modalOpen && (
         <Mailchimp modalOpen={modalOpen} setModalOpen={setModalOpen} />
       )}
-      {/* <div className="form-wrapper">
-        <form action="">
-          <div className="inputs-wrapper">
-            <div className="labelInput">
-              <label htmlFor="">Email</label>
-              <input type="text" />
-            </div>
-            <div className="labelInput">
-              <label htmlFor="">Name</label>
-              <input type="text" />
-            </div>
-            <div className="labelInput">
-              <label htmlFor="">Phone Number</label>
-              <input type="text" />
-            </div>
-          </div>
-          <button className="submitBtn">Submit</button>
-        </form>
-      </div> */}
+<div className="pdfDownload">
+  <a href="dummy.pdf"
+  download='dummy.pdf'>
+  <button>Download</button>
+  </a>
+</div>
     </div>
   );
 }
